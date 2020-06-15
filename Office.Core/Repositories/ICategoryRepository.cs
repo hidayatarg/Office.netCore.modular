7c6d4a407773bd3899ecbeb2f98ea349178da140
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-06-15 22:15:59 +0300

    ICategoryRepository Generic Interface Implemented in Core Layer

---
+++ Office.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Office.Core.Models;
+
+namespace Office.Core.Repositories
+{
+    interface ICategoryRepository: IRepository<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+    }
+}
