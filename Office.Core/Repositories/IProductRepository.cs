064ed0e0504c1e0721ce435aec6a40771ead80e0
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-06-15 22:39:34 +0300

    IProductRepository Generic Interface Implemented in Core Layer

---
+++ Office.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Office.Core.Models;
+
+namespace Office.Core.Repositories
+{
+    internal interface IProductRepository
+    {
+        Task<Product> GetWithCategoryByIdAsync(int productId);
+    }
+}
