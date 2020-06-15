b975f54b1ff2ee854f954ae2a7acbb921a122bc1
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-06-15 21:45:05 +0300

    Created the EntityModels in Core Layer

---
+++ Office.Core/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Office.Core.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            // Default values
+            Products = new Collection<Product>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+
+        // One to Many Relationship for Entity Framework
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Office.Core/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Office.Core.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+
+        // Connection to Category
+        // One to Many Relationship for Entity Framework
+        public virtual Category Category { get; set; }
+
+    }
+}
