3719adc07c8cf8914aa6c7f4e1e922fe0dc76aa7
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-06-15 22:06:52 +0300

    IRepository Generic Interface Implemented in Core Layer

---
+++ Office.Core/Repositories/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Office.Core.Repositories
+{
+    internal interface IRepository<TEntity> where TEntity: class
+    {
+        Task<TEntity> GetByIdAsyn(int id);
+
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task AddAsync(TEntity entity);
+
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+        TEntity Update(TEntity entity);
+    }
+}
