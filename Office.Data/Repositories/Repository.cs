bf827c37b417c55918a181f1f23b00b7c87a15e1
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-06-19 01:37:36 +0300

    Repository Class Implemented in Data Layer

---
+++ Office.Data/Repositories/Repository.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Office.Core.Repositories;
+
+namespace Office.Data.Repositories
+{
+    class Repository<TEntity>: IRepository<TEntity> where TEntity: class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+        
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+        
+        public IEnumerable<TEntity> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+        
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+        
+        public async Task<TEntity> GetByIdAsyn(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+        
+        public async Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbSet.SingleOrDefaultAsync(predicate);
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            // If you do not want to update specific fields
+            // Update the whole Record
+            _context.Entry(entity).State = EntityState.Modified;
+            return entity;
+        }
+    }
+}
