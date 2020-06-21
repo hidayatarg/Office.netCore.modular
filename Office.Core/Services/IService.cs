9d95c17fd55dc9aa2047301bcca78749e378c6ee
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-06-22 01:38:14 +0300

    ProductService Implemented in Service Layer

--- Office.Core/Services/IService.cs
+++ Office.Core/Services/IService.cs
@@ -11,13 +11,13 @@ namespace Office.Core.Services
 
         Task<IEnumerable<TEntity>> GetAllAsync();
 
-        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> Where(Expression<Func<TEntity, bool>> predicate);
 
         Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
 
-        Task AddAsync(TEntity entity);
+        Task<TEntity> AddAsync(TEntity entity);
 
-        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        Task<IEnumerable<TEntity>> AddRangeAsync(IEnumerable<TEntity> entities);
 
         void Remove(TEntity entity);
 
---
+++ Office.Service/Services/ProductService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Office.Core.Models;
+using Office.Core.Services;
+using Office.Core.UnitOfWorks;
+
+namespace Office.Service.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Product> AddAsync(Product entity)
+        {
+            await _unitOfWork.Products.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<Product>> AddRangeAsync(IEnumerable<Product> entities)
+        {
+            await _unitOfWork.Products.AddRangeAsync(entities);
+            await _unitOfWork.CommitAsync();
+            return entities;
+        }
+
+        public async Task<IEnumerable<Product>> Where(Expression<Func<Product, bool>> predicate)
+        {
+            return await _unitOfWork.Products.Where(predicate);
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _unitOfWork.Products.GetAllAsync();
+        }
+
+        public async Task<Product> GetByIdAsyn(int id)
+        {
+            return await _unitOfWork.Products.GetByIdAsyn(id);
+        }
+
+        public async Task<Product> GetWithCategoryByIdAsync(int productId)
+        {
+            return await _unitOfWork.Products.GetWithCategoryByIdAsync(productId);
+        }
+
+        public void Remove(Product entity)
+        {
+            _unitOfWork.Products.Remove(entity);
+            _unitOfWork.Commit();
+        }
+
+        public void RemoveRange(IEnumerable<Product> entities)
+        {
+            _unitOfWork.Products.RemoveRange(entities);
+            _unitOfWork.Commit();
+        }
+
+        public async Task<Product> SingleOrDefaultAsync(Expression<Func<Product, bool>> predicate)
+        {
+            return await _unitOfWork.Products.SingleOrDefaultAsync(predicate);
+        }
+
+        public Product Update(Product entity)
+        {
+            var updateProduct = _unitOfWork.Products.Update(entity);
+            _unitOfWork.Commit();
+            return updateProduct;
+        }
+    }
+}
