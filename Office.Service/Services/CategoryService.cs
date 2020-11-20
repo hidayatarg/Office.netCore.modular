ceba3b5d031c4289cf31044570ccb1befc357ffa
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-11-21 02:33:02 +0300

    Implemented CategoryService in Service Layer

---
+++ Office.Service/Services/CategoryService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Office.Core.Models;
+using Office.Core.Repositories;
+using Office.Core.Services;
+using Office.Core.UnitOfWorks;
+
+namespace Office.Service.Services
+{
+    public class CategoryService : Service<Category>, ICategoryService
+    {
+        public CategoryService(IUnitOfWork unitOfWork, IRepository<Category> repository) : base(unitOfWork, repository)
+        {
+        }
+
+        public async Task<Category> GetWithProductsByIdAsync(int categoryId)
+        {
+            return await _unitOfWork.Categories.GetWithProductsByIdAsync(categoryId);
+        }
+    }
+}
