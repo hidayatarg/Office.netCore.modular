4591a33518ee7340ffdf2fdbf3ae2e5759ea7718
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-11-21 02:19:40 +0300

    Generic Service Class for Events in Service Layer

---
+++ Office.Service/Services/Service.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Office.Core.Repositories;
+using Office.Core.Services;
+using Office.Core.UnitOfWorks;
+
+namespace Office.Service.Services
+{
+    public class Service<TEntity> : IService<TEntity> where TEntity : class
+    {
+        public readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<TEntity> _repository;
+        // dependency injection
+        public Service(IUnitOfWork unitOfWork, IRepository<TEntity> repository)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = repository;
+        }
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await _repository.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<TEntity>> AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            await _repository.AddRangeAsync(entities);
+            await _unitOfWork.CommitAsync();
+            return entities;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsyn(int id)
+        {
+            return await _repository.GetByIdAsyn(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _repository.Remove(entity);
+            // Db method so need to commit
+            _unitOfWork.Commit();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _repository.RemoveRange(entities);
+            _unitOfWork.Commit();
+        }
+
+        public async Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            // singleOrDefault(x=>x.name=="car") perdicate will things in paranthesis
+            return await _repository.SingleOrDefaultAsync(predicate);
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            var updateEntity = _repository.Update(entity);
+            _unitOfWork.Commit();
+            return updateEntity;
+        }
+
+        public async Task<IEnumerable<TEntity>> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _repository.Where(predicate);
+        }
+    }
+}
+
+// ** Note: a logical mistake is done There should be Service class or we need to write the same methods
+// for the product service and the category service.
