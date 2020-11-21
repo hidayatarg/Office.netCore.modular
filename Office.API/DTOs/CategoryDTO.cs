8bfe8a3e7febc0950815c826e45b6f11d60780ad
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-11-21 20:48:52 +0300

    Core API - CategoryDTO Added

---
+++ Office.API/DTOs/CategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Office.API.DTOs
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+    }
+}
+
+// ** Required : Customer should enter the value
