00886397d387ad5f994582a818538bafd1e30f59
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-12-06 04:57:16 +0300

    Core API - GetAll and GetById Methods Implemented in Products Controller

---
+++ Office.API/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Office.API.DTOs;
+using Office.Core.Services;
+
+namespace Office.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        // GET :> Calling the controller will give following
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            // Entites should not be return Data Transfer Objects should be returned
+            var products = await _productService.GetAllAsync();
+            var result = _mapper.Map<IEnumerable<ProductDto>>(products);
+            return Ok(result);
+        }
+
+        //GET :> /api/products/1
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productService.GetByIdAsyn(id);
+            var result = _mapper.Map<ProductDto>(product);
+            return Ok(result);
+        }
+
+    }
+}
